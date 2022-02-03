86b497b406db31eb2bd89cebe16c8aef17713b3a
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-02-03 07:38:18 +0100

    Add project files.

---
+++ Elpris/Program.cs
@@ -0,0 +1,14 @@
+﻿using Elpris;
+
+string? json = JsonCache.Get();
+if (json == null)
+{
+    json = await PriceDownloader.GetTodaysPricesJsonAsync();
+    JsonCache.Put(json);
+}
+
+PriceParser parser = new(json);
+TimeOnly time = new(18, 00);
+DateTime now = DateOnly.FromDateTime(DateTime.Now).ToDateTime(time);
+var price = parser.GetWestPrice(now);
+Console.WriteLine($"Den aktuelle pris er {price} per kWh");
